9b9b163fdfd838d195764662015fe839f8429b28
Nasnin c <nasninc4@gmail.com>
2026-09-13 21:23:58 +0530

    Add HospitalRating DbSet to ApplicationDbContext

---
+++ SmartBed/Models/HospitalRating.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using SmartBed.Models;
+
+namespace SmartBed.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Admin> Admin { get; set; }
+
+        public DbSet<Hospital> Hospital { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<HospitalRating> HospitalRatings { get; set; }
+    }
+}
